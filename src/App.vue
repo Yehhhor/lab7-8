6c7b7284d6774d302de9ed62412c9ebef9faea29
Yehhhor <yehorhadilov@gmail.com>
2024-12-05 06:53:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LotteryItem from "./components/LotteryItem.vue";
+</script>
+
+<template>
+  <main>
+    <LotteryItem />
+  </main>
+</template>
+
+<style scoped></style>
